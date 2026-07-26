80c37a3917b35ab0add28bdfb5d68096c9e5937d
Rouguiyatou Kane Diallo <rouguiyatoud64@gmail.com>
2026-07-26 20:09:47 +0000

    liaison base de donnees en ligne

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-$host = "localhost";
-$db = "mms_db";
-$user = "root";
-$pass = "";
+$host = 'mysql-rouguiyatou.alwaysdata.net'; 
+$dbname = 'rouguiyatou_mms_manager';
+$username = 'rouguiyatou_admin';
+$password = 'VOTRE_MOT_DE_PASSE'; // Remplacez par le mot de passe de l'utilisateur admin créé sur Alwaysdata
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erreur de connexion : " . $e->getMessage());
